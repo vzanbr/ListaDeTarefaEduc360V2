08d4a949c142a0ac4d9eeb60a46e22fa39bb8c7e
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-02-05 11:32:42 -0300

    Feature: Ajustes e correçao de bugs

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.util.List;
+
+import static java.lang.Long.parseLong;
+import static java.lang.Long.valueOf;
 
 public class Main {
 
     public static Scanner leitor = new Scanner(System.in);
-
-    public static void main(String[] args) throws NullPointerException {
+    public static void main(String[] args) {
 
         boolean ativo = true;
         while (ativo) {
@@ -39,7 +43,7 @@ public class Main {
 
     public static void proximaPagina() {
 
-        ArrayList<String> listaTarefas = new ArrayList<>();
+            ArrayList<Tarefa> listaTarefas = new ArrayList<>();
 
         boolean ativo = true;
         while (ativo) {
@@ -47,50 +51,91 @@ public class Main {
             System.out.println("1 - Minhas Tarefas");
             System.out.println("2 - Adcionar Tarefa");
             System.out.println("3 - Remover Tarefa");
-            System.out.println("4 - Organizar Tarefas");
-            System.out.println("5 - Salvar no Computador em formato TXT");
-            System.out.println("6 - Tarefas salvas em TXT Recuperar");
-            System.out.println("8 - Organizar em Cronologica ");
+            System.out.println("4 - Listar Tarefas em Ordem Alfabética");
+            System.out.println("5 - Listar Tarefas em Ordem Cronológica");
+            System.out.println("6 - Salvar no Computador em formato TXT");
             System.out.println("9 - sair");
-            System.out.println("======Fim da Lista======");
+            System.out.println("====================");
             System.out.print("Digite a Opção: ");
             String opcao = leitor.nextLine();
 
-
             switch (opcao) {
-                case "1": {
-                    Tarefa.VerTarefas(listaTarefas);
+                case "1":
+                    for (Tarefa tarefa : listaTarefas)
+                        System.out.println(tarefa);
                     break;
-                }
-                case "2": {
-                    Tarefa.addTarefa(listaTarefas);
+
+                case "2":
+                    Tarefa tarefas = new Tarefa();
+                    System.out.println("=====Tarefa Nova=====");
+                    System.out.print("Digite a descrição: ");
+                    String tarefaResposta = leitor.nextLine();
+
+                    if (tarefaResposta.isEmpty()) {
+                        System.out.println("A descrição da tarefa não pode ser vazia.");
+                        return;
+                    }
+                    tarefas.setNomeTarefa(tarefaResposta);
+                    try {
+                        if (!listaTarefas.contains(tarefas)) {
+                                listaTarefas.add(tarefas);
+                                System.out.println("Tarefa " + tarefaResposta + " adicionada com sucesso!");
+                            } else {
+                                throw new TarefaException("Já existe uma tarefa com o mesmo nome!" + tarefaResposta);
+                            }
+                        } catch(TarefaException e){
+                            System.out.println(e.getMessage());
+                        }
+
+                        for (Tarefa tarefa : listaTarefas)
+                            System.out.println(tarefa);
                     break;
-                }
-                case "3": {
-                    Tarefa.RemoverTarefa(listaTarefas);
+
+                case "3":
+                    System.out.println("Digite o id da tarefa: ");
+                    Scanner entradaRemoverTarefa = new Scanner(System.in);
+                    int removerTarefa = entradaRemoverTarefa.nextInt();
+                    System.out.println("Tem certeza que deseja remover a Tarefa " + removerTarefa + " ?(S / N)");
+                    String escolha = entradaRemoverTarefa.next();
+                    int sizeList = listaTarefas.size();
+
+                    if (escolha.equals("S") || escolha.equals("s"))
+                        listaTarefas.removeIf(tarefa -> tarefa.getId() == removerTarefa);
+
+                        if (listaTarefas.size() < sizeList) {
+                            System.out.println("Tarefa selecionado removida com sucesso");
+                        } else {
+                            System.out.println("Tarefa com ID " + removerTarefa + " não encontrada.");
+                        }
                     break;
-                }
-                case "4": {
-                    Tarefa.organizarTarefasAlfabetico(listaTarefas);
+
+                case "4":
+                    listaTarefas.sort(Tarefa.COMPARATOR_BY_NOME);
+                    System.out.println(listaTarefas);
                     break;
-                }
-                case "5": {
-                    Tarefa.Salvar(listaTarefas);
+
+                case "5":
+                    listaTarefas.sort(Tarefa.COMPARATOR_BY_ID);
+                    System.out.println(listaTarefas);
                     break;
-                }
-                case "6": {
-                    Tarefa.RecuperarLista(listaTarefas);
+
+                case "6":
+                    System.out.println("Escolha um nome para o arquivo:");
+                    String nomeArquivo = leitor.nextLine();
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo + ".txt"))) {
+                        for (Tarefa tarefa : listaTarefas) {
+                            writer.write(tarefa.getId());
+                            writer.newLine();
+                        }
+                    } catch (IOException e) {
+                        System.out.println("erro");
+                    }
                     break;
-                }
-                case "8": {
-                    Tarefa.organizarTarefa(listaTarefas);
-                    break;
-                }
-                case "9": {
+
+                case "9":
                     ativo = false;
                     System.out.println("Saindo da lista!");
                     break;
-                }
 
                 default:
                     System.out.println("Comando invalido");
--- src/Tarefa.java
+++ src/Tarefa.java
@@ -1,114 +1,58 @@
-import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
-public class Tarefa {
+public class Tarefa  {
 
-    public static void addTarefa(ArrayList<String> listaTarefas) {
+    private static int moreId = 0;
 
-        Scanner leitor = new Scanner(System.in);
-        System.out.println("=====Tarefa Nova=====");
-        System.out.print("Digite a descrição: ");
-        String tarefaResposta = leitor.nextLine();
-        {
-            if (!listaTarefas.contains(tarefaResposta)) {
-                listaTarefas.add(tarefaResposta);
-                System.out.println("Tarefa " + tarefaResposta + " adicionada com sucesso!");
-            } else {
-                System.out.println("Ja existe tarefa com o mesmo nome!");
-            }
+    private int id;
+    private String nomeTarefa;
 
-        }
+    public Tarefa() {
+        this.id = moreId++;
     }
 
-    public static void RemoverTarefa(ArrayList<String> listaTarefas){
-
-        for (int i = 1; i-1 < listaTarefas.size(); i++) {
-            System.out.println(i + "- " + listaTarefas.get(i-1));
-        }
-        System.out.print("Digite o número da tarefa que deseja remover: ");
-        Scanner entradaRemoverTarefa = new Scanner(System.in);
-        int removerTarefa = entradaRemoverTarefa.nextInt();
-        boolean verificador = listaTarefas.size() < removerTarefa;
-        if (verificador) {
-            System.out.println("Lista não possui nenhuma tarrefa com esse número");
-        } else {
-            System.out.println("Deseja realmente excluir a tarefa " + removerTarefa + "? Escreva Com S maiusculo Para confirmar");
-            String resposta = entradaRemoverTarefa.next();
-            if (resposta.equals("S")) {
-                listaTarefas.remove(removerTarefa - 1);
-                System.out.println("tarefa " + removerTarefa + " Removida com sucesso!");
-                System.out.println("Lista : " + listaTarefas);
-            } else {
-                System.out.println("Falha para excluir " + resposta + " tarefa!");
-            }
-        }
-    }
-    public static void VerTarefas(ArrayList<String> listaTarefas){
-        for (String tarefa : listaTarefas) {
-            System.out.println(tarefa);
-        }
+    public Tarefa (int id, String nomeTarefa) {
+        this.id = id;
+        this.nomeTarefa = nomeTarefa;
     }
 
-    public static void organizarTarefasAlfabetico (ArrayList<String> listaTarefas) {
-        Collections.sort(listaTarefas);
-        for (String listaTarefa : listaTarefas) {
-            System.out.println(listaTarefa);
-        }
+    public int getId() {
+        return id;
     }
 
-    public static void organizarTarefa (ArrayList<String> listaTarefas) {
-        System.out.println("=======Tarefas organizadas=====" );
-            Collections.sort(listaTarefas);
-        System.out.println(listaTarefas);
-            System.out.println("=======Tarefas organizadas=====" );
-        }
+    public String getNomeTarefa() {
+        return nomeTarefa;
+    }
 
-//    public static void AlterarStatus(ArrayList<String> listaTarefas){
-//        for (int i = 1; i-1 < listaTarefas.size(); i++) {
-//            System.out.println(i + "- " + listaTarefas.get(i-1));
-//        }
-//        System.out.print("Digite o número da tarefa que deseja altrar o status: ");
-//        Scanner entradaAlterarTarefa = new Scanner(System.in);
-//        int alterarTarefa = entradaAlterarTarefa.nextInt();
-//        if (listaTarefas.size() < alterarTarefa) {
-//            System.out.println("[ERRO] número invalido");
-//        } else {
-//            System.out.println("Status alterado com sucesso");
-//            if (listaTarefas.get(alterarTarefa - 1).charAt(1) == 'X') {
-//                listaTarefas.set(alterarTarefa - 1, "[]" + listaTarefas.get(alterarTarefa - 1).substring(3));
+    public void setNomeTarefa(String nomeTarefa) {
+        this.nomeTarefa = nomeTarefa;
+    }
+
+//    public static void addTarefa(ArrayList<String> listaTarefas) {
+//
+//        Scanner leitor = new Scanner(System.in);
+//        System.out.println("=====Tarefa Nova=====");
+//        System.out.print("Digite a descrição: ");
+//        String tarefaResposta = leitor.nextLine();
+//        {
+//            if (!listaTarefas.contains(tarefaResposta)) {
+//                listaTarefas.add(tarefaResposta);
+//                System.out.println("Tarefa " + tarefaResposta + " adicionada com sucesso!");
 //            } else {
-//                listaTarefas.set(alterarTarefa - 1, " [X] " + listaTarefas.get(alterarTarefa - 1).substring(3));
+//                System.out.println("Ja existe tarefa com o mesmo nome!");
 //            }
+//
 //        }
 //    }
-    public static void Salvar(ArrayList<String> listaTarefas){
-        System.out.println("Escolha um nome para o arquivo:");
-        Scanner entradaNomeArquivo = new Scanner(System.in);
-        String nomeArquivo = entradaNomeArquivo.nextLine();
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo + ".txt"))) {
-            for (String tarefa : listaTarefas) {
-                writer.write(tarefa);
-                writer.newLine();
-            }
-        } catch (IOException e) {
-            System.out.println("erro");
-        }
+
+
+    @Override
+    public String toString() {
+        return "Tarefas{" +  "id=" + id +  ", nomeTarefa='" + nomeTarefa + '\'' + '}';
     }
-    public static void RecuperarLista(ArrayList<String> listaTarefas){
-        System.out.println("Digite o nome da lista que você quer recuperar:");
-        Scanner entradaNomeArquivo = new Scanner(System.in);
-        String nomeArquivo = entradaNomeArquivo.nextLine();
-        try (BufferedReader reader =new BufferedReader(new FileReader(nomeArquivo + ".txt"))) {
-            String linha;
-            while ((linha = reader.readLine()) != null) {
-                listaTarefas.add(linha);
-            }
-        }catch (IOException e) {
-            System.out.println("erro");
-        }
-    }
+
+    public static final Comparator<Tarefa> COMPARATOR_BY_NOME = Comparator.comparing(Tarefa::getNomeTarefa);
+    public static final Comparator<Tarefa> COMPARATOR_BY_ID = Comparator.comparing(Tarefa::getId);
 }
