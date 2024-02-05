41cf0799a6016e3e66866d8647eb6689bfd0ee06
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-02-05 11:45:53 -0300

    Feature: Ajustes e correçao de bugs

--- src/Tarefa.java
+++ src/Tarefa.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Scanner;
 
 public class Tarefa  {
 
---
+++ src/TarefaException.java
@@ -0,0 +1,7 @@
+
+public class TarefaException extends Exception{
+
+    public TarefaException(String mesnagem){
+        super(mesnagem);
+    }
+}
